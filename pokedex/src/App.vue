999de6f8c334860cb944baa75d3741940d1e8d5f
Rodrigo Gomes <cklrod@hotmail.com>
2023-02-25 23:15:26 -0300

    POkedex criada, filtro criado

---
+++ pokedex/src/App.vue
@@ -0,0 +1,120 @@
+<template>
+  <v-app>
+    <pokedex-menu :filter-value="filterValue" @filter-pokemons="filterPokemons" />
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="3" v-for="pokemon in filteredPokemons" :key="pokemon.name">
+          <v-card class="gradient">
+            <v-img
+              :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${getId(pokemon)}.gif`"
+              contain class="responsive-image"></v-img>
+            <v-card-title class="d-flex justify-center custom-title" align="center">{{ pokemon.name }}</v-card-title>
+            <v-card-subtitle v-for="(type, index) in getType(pokemon)" :key="index">{{ type.type.name }}</v-card-subtitle>
+            <v-card-text>{{ pokemon.description }}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+import axios from 'axios';
+import PokedexMenu from '@/components/Menu.vue';
+export default {
+  name: 'App',
+
+  components: {
+    PokedexMenu,
+  },
+
+  data() {
+    return {
+      pokemons: [],
+      filterValue: "",
+    }
+  },
+
+  computed: {
+    filteredPokemons() {
+      if (this.filterValue) {
+        return this.pokemons.filter((pokemon) =>
+          pokemon.name.toLowerCase().includes(this.filterValue)
+        );
+      } else {
+        return this.pokemons;
+      }
+    },
+  },
+
+  mounted() {
+    axios.get(" https://pokeapi.co/api/v2/pokemon?limit=300").then((response) => {
+      this.pokemons = response.data.results
+    })
+
+  },
+
+  methods: {
+    filterPokemons(value) {
+      this.filterValue = value;
+    },
+
+    getId(pokemon) {
+      return pokemon.url.split("/")[6]
+    },
+
+    async getType(pokemon) {
+      try {
+        let id = this.getId(pokemon)
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const result = response.data;
+
+        // Para acessar o tipo do Pokemon, você pode usar a propriedade "types" do objeto retornado.
+        // const pokemonTypes = result.types.map(type => type.type.name);
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+
+};
+</script>
+
+<style>
+#app {
+  background: linear-gradient(to bottom, #a40808, #e73939) no-repeat center center fixed !important;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
+}
+
+.gradient {
+  background: linear-gradient(to bottom, #4b6cb7, #182848);
+  border: solid 3px transparent;
+  background-clip: padding-box;
+  padding-top: 2rem;
+}
+
+.gradient:before {
+  content: '';
+  position: absolute;
+  top: -3px;
+  left: -3px;
+  right: -3px;
+  bottom: -3px;
+  background: linear-gradient(to bottom, #6e7277, #434649) !important;
+  z-index: -1;
+}
+
+.responsive-image {
+  max-width: 100%;
+  max-height: 80px;
+}
+
+.custom-title {
+  text-transform: capitalize;
+  color: #fff !important;
+}
+</style>
---
+++ pokedex/src/components/Menu.vue
@@ -0,0 +1,124 @@
+<template>
+    <v-app-bar app color="#ff0000" class="menu-bar">
+
+
+        <div class="d-flex">
+            <v-avatar class="blue-gradient">
+            </v-avatar>
+            <div class="d-flex">
+                <v-avatar class="red-gradient blinking-red">
+                </v-avatar>
+                <v-avatar class="yellow-gradient blinking-yellow">
+                </v-avatar>
+                <v-avatar class="green-gradient blinking-green ">
+                </v-avatar>
+            </div>
+        </div>
+        <v-responsive class="mx-auto" max-width="200">
+            <v-text-field v-model="search" append-icon="mdi-magnify" dark label="Pesquisar" @input="filterPokemons"
+                placeholder="Nome do Pokémon" hide-details solo-inverted @keydown.enter="filterPokemons">
+            </v-text-field>
+        </v-responsive>
+
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'PokedexMenu',
+
+    data() {
+        return {
+            drawer: false,
+            search: ''
+        }
+    },
+    methods: {
+        searchPokemon() {
+            // Implementar a busca aqui, por exemplo:
+            console.log(`Buscando por ${this.search}`);
+        },
+        filterPokemons() {
+            this.$emit("filter-pokemons", this.search.toLowerCase());
+        },
+    },
+}
+</script>
+
+<style>
+v-toolbar__content,
+.v-toolbar__extension {
+    align-items: start !important;
+    padding: 0;
+}
+
+.v-avatar {
+    height: 15px !important;
+    min-width: 15px !important;
+    width: 15px !important;
+    margin: 0 0.2rem;
+}
+
+.blue-gradient {
+    background: linear-gradient(to bottom, #5fb5f2, #0798ff);
+    border: solid 2px #fff;
+    height: 50px !important;
+    min-width: 50px !important;
+    width: 50px !important;
+}
+
+.yellow-gradient {
+    background: linear-gradient(to bottom, #f2c45f, #ffc107);
+}
+
+.green-gradient {
+    background: linear-gradient(to bottom, #81f25f, #5eff07);
+}
+
+.red-gradient {
+    background: linear-gradient(to bottom, #722b2b, #8b0404);
+}
+
+@keyframes redblink {
+    50% {
+        background: #804646;
+        border: solid 2px #8b0404;
+    }
+}
+
+@keyframes yellowblink {
+    50% {
+        background: #fbd564;
+        border: solid 2px #ffc107;
+    }
+}
+
+@keyframes greenblink {
+    50% {
+        background: #b0f28c;
+        border: solid 2px #5eff07;
+    }
+}
+
+.blinking-red {
+    animation: redblink 2s infinite;
+}
+
+.blinking-yellow {
+    animation: yellowblink 3s infinite;
+}
+
+.blinking-green {
+    animation: greenblink 4s infinite;
+}
+
+.menu-bar {
+    position: relative !important;
+    flex: none;
+    color: #fff;
+}
+
+.text-decoration-none {
+    text-decoration: none;
+}
+</style>
